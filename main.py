d0115e72b0491153ff7376815f05fc810a00ea3a
Kverde <Kverde@yandex.ru>
2017-01-15 12:24:04 +0500

    add botan, refactoring setting

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
-import configparser
-import os
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-from source.ks_math import MathParser
+from lib import botan
+from lib.setting import Setting
+from source.domain import Domain
 
 APP_ID = 'IceCalcBot'
 
-def loadTelegramToken():
-    token = os.getenv('TELEGRAM_TOKEN')
-    if not token is None:
-        return token
+setting = Setting(APP_ID)
+domain = Domain()
 
-    setting_path = os.getenv('ice_setting')
-    if setting_path is None:
-        raise Exception('System var ice_setting not found')
+def botanTrack(message, event_name):
+    if setting.botan_token == '':
+        return
 
-    settingFileName = os.path.join(setting_path, APP_ID, 'setting.ini')
+    uid = message.from_user
+    message_dict = message.to_dict()
+    botan.track(setting.botan_token, uid, message_dict, event_name)
 
-    if not os.path.exists(settingFileName):
-        raise Exception('Setting file {} not found'.format(settingFileName))
 
-    config = configparser.ConfigParser()
-    config.read(settingFileName)
+def cm_start(bot, update):
+    domain.on_start(bot, update)
+    botanTrack(update.message, 'start')
 
-    return config['main']['telegram_token']
+def cm_help(bot, update):
+    domain.on_help(bot, update)
+    botanTrack(update.message, 'help')
 
-telegramToken = loadTelegramToken()
-
-def start(bot, update):
-    update.message.reply_text('Привет! Отправьте мне математическое выражение и я верну результат. Для получения помощи введите "help" или "?".')
-    send_help(bot, update)
-
-def send_help(bot, update):
-    update.message.reply_text('Поддерживаемые операции +, -, *, /, ^')
-    update.message.reply_text('Для группировки выражений доступны скобки')
-
-def help(bot, update):
-    send_help(bot, update)
-
-def text(bot, update):
+def callb_text(bot, update):
     try:
-        text = update.message.text.strip()
-        if text == '?' or text == 'help':
-            send_help(bot, update)
-            return
-
-        res = MathParser(text).parse()
-        update.message.reply_text(res)
+        log_msg = domain.on_text(bot, update)
+        botanTrack(update.message, log_msg)
     except Exception as e:
         print(e)
 		
-updater = Updater(loadTelegramToken())
+updater = Updater(setting.telegram_token)
 
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, text))
+updater.dispatcher.add_handler(CommandHandler('start', cm_start))
+updater.dispatcher.add_handler(CommandHandler('help', cm_help))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, callb_text))
 
 updater.start_polling()
 updater.idle()
---
+++ source/domain.py
@@ -0,0 +1,22 @@
+from source.ks_math import MathParser
+
+class Domain():
+    def on_start(self, bot, update):
+        update.message.reply_text(
+            'Привет! Отправьте мне математическое выражение и я верну результат. Для получения помощи введите "help" или "?".')
+
+        self.on_help(bot, update)
+
+    def on_help(self, bot, update):
+        update.message.reply_text('Поддерживаемые операции +, -, *, /, ^')
+        update.message.reply_text('Для группировки выражений доступны скобки')
+
+    def on_text(self, bot, update):
+        text = update.message.text.strip()
+        if text == '?' or text == 'help':
+            self.on_help(bot, update)
+            return 'text_help'
+
+        res = MathParser(text).parse()
+        update.message.reply_text(res)
+        return 'math'
